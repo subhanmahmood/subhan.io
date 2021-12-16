dd1e4da26135e099b87658a3865a49c1c4aa976a
Subhan Mahmood <sm01800@surrey.ac.uk>
2021-12-16 17:01:14 +0000

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,17 +2,14 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-700 text-white w-full py-32">
+        <footer className="bg-gray-700 text-white w-full py-24">
             <div className="container mx-auto">
                 <div className="w-8/12 mx-auto">
                     <div className="flex flex-col items-end w-full space-y-8">
-                        <div className="flex flex-row justify-between w-full">
+                        <div className="flex flex-row items-center justify-between w-full space-x-8">
                             <img src="/icons/footer-logo.svg" />
                             <div className="flex flex-col gap-y-2">
-                                <p className="font-semibold text-lg">Links</p>
-                                <a>About</a>
-                                <a>My Work</a>
-                                <a>Contact</a>
+                                <p className="font-regular font-display text-md">Made by Subhan Mahmood</p>
                             </div>
                         </div>
                     </div>
